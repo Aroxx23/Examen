92500094eea7d526eb1b33d52e23d007cc543402
Alex <correo>
2025-03-12 20:25:57 +0100

    Tercer commoijlmit

---
+++ Examen.md
@@ -0,0 +1,3 @@
+# Examen
+## Primera función
+Esta primra funcion llamada _buscarPalabra_ hay que pasarle
--- Examen.py
+++ Examen.py
@@ -1,18 +1,5 @@
 def buscarPalabra(objetivo, palabras):
-    objetivo
-
-
-def imprimirListaInversa(lista):
-
-
-nombres = ["Mengano", "Fulano", "Zutano", "Perantano"]
-edades = {
-    "Mengano": 0,
-    "Fulano": 25,
-    "Zutano": 50,
-    "Perantano": 75
-}
-while true:
+   while true:
     try:
         objetivo = str(input("Buscar nombre: "))
 
@@ -30,7 +17,30 @@ while true:
 
     elif objetivo == "exit"
     break
-    
+
+    else:
+        print("El nombre no existe...")
+
     print("\nFIN DEL PROGRAMA")
 
+
+def imprimirListaInversa(lista):
+    lista = []
+     for i in range(len(nombres)-1, -1, -1):
+ 
+     lista_invertida.append(nombres[i])
+
+     return lista
+
+nombres = ["Mengano", "Fulano", "Zutano", "Perantano"]
+edades = {
+    "Mengano": 0,
+    "Fulano": 25,
+    "Zutano": 50,
+    "Perantano": 75
+}
+
+
+
+print(imprimirListaInversa("Mengano", "Fulano", "Zutano", "Perantano"))
 
